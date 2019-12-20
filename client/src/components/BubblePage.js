634e2be1547d8708af4aeb8d66aebf80f2505d2f
toddmurphy <toddmurphy93@gmail.com>
2019-12-20 13:41:49 -0400

    bubbles displaying in BubblePage

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import {axiosWithAuth} from '../utils/axiosWithAuth';
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +8,20 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+
+  useEffect(() => {
+      axiosWithAuth()
+        .get('/colors')
+        .then(response => {
+          console.log(response.data)
+
+          setColorList(response.data)
+        })
+        .catch(error => {
+          console.log('Sorry, No colors found!', error)
+        })
+
+  },[])
 
   return (
     <>
@@ -18,3 +32,9 @@ const BubblePage = () => {
 };
 
 export default BubblePage;
+
+/* Data structure from api
+color: "aliceblue"
+code: {hex: "#f0f8ff"}
+id: 1 
+*/
