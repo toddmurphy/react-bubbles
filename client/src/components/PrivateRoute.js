271a25c52de1eb7ab7731d0a13a3f3b4c6ddf48b
toddmurphy <toddmurphy93@gmail.com>
2019-12-20 12:32:15 -0400

    axiosWithAuth function made

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrivateRoute = () => {
+    return(
+        <div>
+            <h3>Private route component</h3>
+        </div>
+    )
+}
+
+export default PrivateRoute;
